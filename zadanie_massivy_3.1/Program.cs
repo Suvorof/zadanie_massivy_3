b045a96bb0e5594ca8a3edc1ef8dcd8702d51846
Konstantin Suvorov <konsu@inbox.ru>
2018-05-09 18:46:57 +0300

    Добавьте файлы проекта.

---
+++ zadanie_massivy_3.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_massivy_3._1
+{
+    class Program
+    {
+        public static int[] Add(int[] array, int value)
+        {
+            Array.Resize<int>(ref array, array.Length + 1);
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                array[0] = value;
+                Console.Write(array[i] + " ");
+            }
+            return array;
+        }
+    
+        static void Main()
+        {
+            int[] massiv = new int[7] { 1, 2, 3, 4, 5, 6, 7 };
+            Add(massiv, 9);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ zadanie_massivy_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_massivy_3
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            int[] array = new int[7] { 1, 2, 3, 4, 5, 6, 7 }; //создаем целый массив из 7-ми элементов
+            for (int i = 0; i <= array.Length-1; i++)
+                Console.Write(array[i] + " ");
+            Console.WriteLine();
+
+            Array.Resize<int>(ref array, array.Length + 1); //увеличиваем его размер на 1
+            for (int i = 0; i <= array.Length-1; i++)
+                Console.Write(array[i] + " ");
+            Console.ReadKey();
+        }
+    }
+}
